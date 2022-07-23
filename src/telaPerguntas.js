83034631ba6f7f6dcd201cecf8d4cdbdb291836d
Ana Carvalho <anapaulacmoura@gmail.com>
2022-07-23 19:06:46 -0300

    adicionando funcionalidade de perguntas dinâmicas

--- src/telaPerguntas.js
+++ src/telaPerguntas.js
@@ -2,14 +2,21 @@ import Header from "./header";
 import Footer from "./Footer";
 
 function TelaPerguntas(){
+
+    const perguntas = ['Pergunta 1', 'Pergunta 2', 'Pergunta 3', 'Pergunta 4'];
+
     return (
         <div className="TelaPerguntas">
         <Header />
 
         <div className="content">
-        <ul className="pergunta">
-            <li>Pergunta 1</li>
-            <img src="./imgs/seta.svg" alt="botão seta" />
+        <ul className="perguntas">
+            
+
+            {perguntas.map(pergunta => <li className="pergunta">{pergunta}
+            <img src="./imgs/seta.svg" alt="botão seta" /></li>)}
+            
+            
         </ul>
        
         </div>
